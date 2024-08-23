6c54de6c41e98303797783a9396c37b9ff384a82
begum-aydinatay <132909653+begum-aydinatay@users.noreply.github.com>
2024-08-23 09:19:49 +0200

    Added seed data for practice.

---
+++ NLayer.Repository/Seeds/ProductFeatureSeed.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductFeatureSeed : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasData(
+                new ProductFeature
+                {
+                    Id = 1,
+                    Color = "Brown",
+                    Height = 100,
+                    Width = 5,
+                    ProductId = 1,
+                },
+                new ProductFeature
+                {
+                    Id = 2,
+                    Color = "Yellow",
+                    Height = 300,
+                    Width = 10,
+                    ProductId = 3,
+                }
+            );
+        }
+    }
+}
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Name = "Mitsubishi 9852EW HB",
+                    Price = 10,
+                    Stock = 100,
+                    CreatedDate = DateTime.Now,
+                },
+                new Product
+                {
+                    Id = 2,
+                    CategoryId = 2,
+                    Name = "Faber Castell A4",
+                    Price = 5,
+                    Stock = 30,
+                    CreatedDate = DateTime.Now,
+                },
+                new Product
+                {
+                    Id = 3,
+                    CategoryId = 3,
+                    Name = "Sharpi Accent",
+                    Price = 15,
+                    Stock = 10,
+                    CreatedDate = DateTime.Now,
+                }
+            );
+        }
+    }
+}
