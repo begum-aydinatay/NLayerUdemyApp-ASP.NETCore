afdcd11dbab531fcb5c59084d88c121b73b2524f
begum-aydinatay <132909653+begum-aydinatay@users.noreply.github.com>
2024-08-26 09:57:59 +0200

    Added the entity <=> DTO mapper.

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -8,6 +8,8 @@ using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Repository;
 using NLayer.Repository.Repositories;
+using NLayer.Service.Services;
+using NLayer.Service.Mapping;
 
 namespace NLayer.API
 {
@@ -25,6 +27,8 @@ namespace NLayer.API
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+            builder.Services.AddAutoMapper(typeof(MapProfile));
 
             builder.Services.AddDbContext<AppDbContext>(x =>
             {
