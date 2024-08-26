6d5c378ed2568d8b7923ecc4ce9672c4ab359478
begum-aydinatay <132909653+begum-aydinatay@users.noreply.github.com>
2024-08-26 10:34:03 +0200

    Added custom response DTO.

---
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public List<String> Errors { get; set; }
+
+        //Static factory methods
+
+        public static CustomResponseDto<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDto<T> { Data = data, StatusCode = statusCode };
+        }
+
+        //when you don't want to return any data
+        public static CustomResponseDto<T> Success(int statusCode) 
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode };
+        }
+
+        public static CustomResponseDto<T> Failure(int statusCode, List<string> errors)
+        {
+            return new CustomResponseDto<T> { Errors = errors, StatusCode = statusCode };
+        }
+
+        public static CustomResponseDto<T> Failure(int statusCode, string error)
+        {
+            return new CustomResponseDto<T> { Errors = new List<string> { error }, StatusCode = statusCode };
+        }
+    }
+}
