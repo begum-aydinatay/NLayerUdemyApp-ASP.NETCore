9c93ba7966b6bae8b4b0b98d526e7a72a0749aa0
begum-aydinatay <132909653+begum-aydinatay@users.noreply.github.com>
2024-08-27 10:39:04 +0200

    Added custom validator for product DTO.

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -10,6 +10,10 @@ using NLayer.Repository;
 using NLayer.Repository.Repositories;
 using NLayer.Service.Services;
 using NLayer.Service.Mapping;
+using FluentValidation.AspNetCore;
+using NLayer.Service.Validations;
+using NLayer.API.Filters;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NLayer.API
 {
@@ -21,7 +25,11 @@ namespace NLayer.API
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(options => options.Filters.Add(new ValidateFilterAttribute())).AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
