13b2c83d6683bd8d071b176fdc1489dca1d6ad27
begum-aydinatay <132909653+begum-aydinatay@users.noreply.github.com>
2024-08-28 12:47:53 +0200

    Added validation for the 'Save' view properties.

--- NLayer.Web/Program.cs
+++ NLayer.Web/Program.cs
@@ -1,9 +1,11 @@
 using System.Reflection;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using NLayer.Repository;
 using NLayer.Service.Mapping;
+using NLayer.Service.Validations;
 using NLayer.Web.Modules;
 
 namespace NLayer.Web
@@ -15,7 +17,7 @@ namespace NLayer.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
             builder.Services.AddAutoMapper(typeof(MapProfile));
 
             builder.Services.AddDbContext<AppDbContext>(x =>
